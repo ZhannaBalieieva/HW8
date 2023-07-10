b13ce67ef1a72f617a3c4fb4c33a5c51b1aada29
ZhannaBalieieva <119054020+ZhannaBalieieva@users.noreply.github.com>
2023-07-10 19:18:55 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -12,12 +12,7 @@ db_name = config.get('DB', 'db_name')
 domain = config.get('DB', 'domain')
 
 # connect to cluster on AtlasDB with connection string
-
 #######connect(host="mongodb://127.0.0.1:27017/my_db")##########
-
-
-
-
 connect(host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""", ssl=True)
 
 
